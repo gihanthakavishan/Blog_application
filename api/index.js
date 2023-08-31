c946f9fa3ff73c2528e9d8313bd1e06023170446
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-08-31 18:06:35 +0530

    updated files

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 const express= require ('express');
 const app = express();
+const app =require('cors');y
+
+
+app.arguments(cors());
+app.arguments(express.json());
 
 app.post('/register',(req,res)=> {
-    res.json('test ok');
+    const {username,password} = req.body;
+    res.json({requestData:{username,password}});
 });
 
 app.listen(4000, () => {
