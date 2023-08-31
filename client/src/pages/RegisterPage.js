2a71c9e603bbbba503eeddedc018db4c040f5789
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-08-31 17:19:01 +0530

    change registerpage

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -5,8 +5,8 @@ export default function RegisterPage (){
     return (
         <form className="register">
             <h1>Register</h1>
-            <input type="text" placeholder="username"></input>
-            <input type="password" placeholder="password"></input>
+            <input type="text" placeholder="username" value={username}></input>
+            <input type="password" placeholder="password" ></input>
             <button>Register</button> 
         </form>
     );
