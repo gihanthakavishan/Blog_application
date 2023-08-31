8fb7105701a8d0dfdba5fcb1c8eed482b2453fea
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-08-31 17:43:25 +0530

    add new function

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -2,12 +2,21 @@ import {useState} from "react";
 export default function RegisterPage (){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    return (
-        <form className="register">
+   async function register(ev){
+        ev.preventDefault();
+        await fetch('http://localhost:4000',{
+         method: 'POST',
+         body:JSON.stringify({username,password}),
+         headers: {'Content-Type':'application/json'},
+
+     } )
+    }
+        return (
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
             <input type="text" placeholder="username" value={username} onChange={ev =>setUsername(ev.target.value)}></input>
             <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}></input>
-            <button>Register</button> 
+            <button>Register</button>
         </form>
     );
 }
