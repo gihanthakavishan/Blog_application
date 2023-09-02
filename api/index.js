7dfdb29f8e53bd85bf96d8f1fd4232a562ce409e
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-09-02 16:38:17 +0530

    modified files

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors');
 const mongoose = require("mongoose");
 const User = require('./models/User');
-
+const bcrypt = require('bcrypt');
+const salt = 'hgugtrgojbnmcbdsc7362xnyn453HJH';
 app.use(cors());
 app.use(express.json());
 
@@ -12,7 +13,9 @@ mongoose.connect('mongodb+srv://blog:1234@cluster0.ivaexdy.mongodb.net/pin?retry
 app.post('/register', async (req,res)=> {
     const {username,password} = req.body;
     try{
-        const userDoc = await User.create({username,password});
+        const userDoc = await User.create({username,
+            password:bcrypt.hashSync(password,salt),
+        });
         res.json(userDoc);
     }catch(e){
         res.status(400).json(e);
