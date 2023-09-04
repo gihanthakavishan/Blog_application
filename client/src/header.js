85ce359dfe48cfaab9484139babad7f1e5b8faff
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-09-04 18:25:25 +0530

    add changes

--- client/src/header.js
+++ client/src/header.js
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header(){
+    const [username, setUsername] = useState(null);
     useEffect(()=> {
       fetch('http://localhost:4000/profile',{
         credentials: 'include',
-      })
+      }).then(response =>{
+         response.json().then(userInfo =>{
+             
+         });
+      });
     }, []);
     return (<header>
         <Link to="/" className="logo">Myblog</Link>
