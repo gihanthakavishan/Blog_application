3d38cbe04a27dcb3b89fd1244d97d5b070fb7dd5
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-09-06 13:12:07 +0530

    correct some errors

--- client/src/header.js
+++ client/src/header.js
@@ -1,59 +1,113 @@
-import { useContext, useEffect, useState } from "react";
+// import { useContext, useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import { UserContext } from "./userContext";
+// // import React from "react";
+// // import ReactDOM from "react-dom";
+// // import App from "./App"; // Your main application component
+// // import { UserContextProvider } from "./userContext"; // Import your UserContextProvider
+
+// // ReactDOM.render(
+// //     <React.StrictMode>
+// //         <UserContextProvider>
+// //             <App />
+// //         </UserContextProvider>
+// //     </React.StrictMode>,
+// //     document.getElementById("root")
+// // );
+
+// export default function Header(){
+   
+//     const [userInfo, setUserinfo] = useContext(null);
+//     const [username, setUsername] = useState(null);
+//     useEffect(()=> {
+//       fetch('http://localhost:4000/profile',{
+//         credentials: 'include',
+//       }).then(response =>{
+//          response.json().then(userInfo =>{
+//             console.log(userInfo)
+//             // setUserinfo(userInfo); 
+//              setUsername(userInfo.username); 
+//          });
+//       });
+//     }, []);
+
+//     function logout() {
+//     fetch('http://localhost:4000/logout',{
+//         credentials: 'include',
+//         method: 'POST',
+//     });
+//     setUserinfo(null);
+//     }
+
+//     const usersname = userInfo?.usersname;
+//     return (<header>
+//         <Link to="/" className="logo">Myblog</Link>
+//         <nav>
+//             {username && (
+//                 <>
+//                 <Link to="/create">Create new post</Link>
+//                 <a onClick={logout}>Logout</a>
+//                 </>
+//             )}
+//             {!username && (
+//                 <>
+//                  <Link to="/login">Login</Link>
+//                  <Link to="/register">Register</Link>
+//             </>
+//             )}
+//         </nav>
+//        </header>);
+// }
+
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./userContext";
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App"; // Your main application component
-// import { UserContextProvider } from "./userContext"; // Import your UserContextProvider
 
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <UserContextProvider>
-//             <App />
-//         </UserContextProvider>
-//     </React.StrictMode>,
-//     document.getElementById("root")
-// );
+export default function Header() {
+  const [userInfo, setUserinfo] = useState(null); // Initialize with null or initial value
+  const [username, setUsername] = useState(null);
 
-export default function Header(){
-    const [userInfo, setUserinfo] = useContext(UserContext);
-    const [username, setUsername] = useState(null);
-    useEffect(()=> {
-      fetch('http://localhost:4000/profile',{
-        credentials: 'include',
-      }).then(response =>{
-         response.json().then(userInfo =>{
-            console.log(userInfo)
-            setUserinfo(userInfo); 
-             //setUsername(userInfo.username); 
-         });
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userData => {
+        console.log(userData);
+        setUserinfo(userData); // Update userInfo state
+        setUsername(userData.username);
       });
-    }, []);
-
-    function logout() {
-    fetch('http://localhost:4000/logout',{
-        credentials: 'include',
-        method: 'POST',
     });
-    setUserinfo(null);
-    }
+  }, []);
 
-    const usersname = userInfo?.usersname;
-    return (<header>
-        <Link to="/" className="logo">Myblog</Link>
-        <nav>
-            {username && (
-                <>
-                <Link to="/create">Create new post</Link>
-                <a onClick={logout}>Logout</a>
-                </>
-            )}
-            {!username && (
-                <>
-                 <Link to="/login">Login</Link>
-                 <Link to="/register">Register</Link>
-            </>
-            )}
-        </nav>
-       </header>);
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserinfo(null); // Clear userInfo when logging out
+  }
+
+  const usersname = userInfo?.usersname;
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        Myblog
+      </Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create a new post</Link>
+            <a onClick={logout}>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }
