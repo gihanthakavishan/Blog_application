e850a1e8d69d43f99d0cf3707ec5b7e4ad47c9d7
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-09-14 18:26:37 +0530

    create post area

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,7 +1,10 @@
 export default function CreatePost(){
     return(
-        <div>
-            Here you create your new post
-        </div>
+        <form>
+           <input type="title" placeholder={'Title'}/>
+           <input type="summary" placeholder={'Summary'}/>
+           <input type="file"/>
+           <textarea name="" id="" cols="30" rows="10"></textarea>
+           </form>
     );
 }
