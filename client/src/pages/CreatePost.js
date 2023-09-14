14568b851a306e9052ffb4400a8025a2e631a950
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-09-14 19:44:48 +0530

    add some codes

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 export default function CreatePost(){
+    const[title,setTitle] = useState('');
+    const[summary,setSummary] = useState('');
+    const[content,setContent] = useState('');
+    const modules ={
+        toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+],
+    };
     return(
         <form>
            <input type="title" placeholder={'Title'}/>
            <input type="summary" placeholder={'Summary'}/>
            <input type="file"/>
-           <ReactQuill/>
+           <ReactQuill value={content}/>
            <button style={{marginTop:'5px'}}> Create post</button>
            </form>
     );
