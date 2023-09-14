802e14d50a1510be3149346f2eabb72a5495f1b8
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-09-14 23:18:32 +0530

    do changes

--- api/index.js
+++ api/index.js
@@ -96,13 +96,21 @@ app.post('/logout',(req,res)=>{
     res.cookie('token', '').json('ok');
 })
 
-app.post('/post', uploadMiddleware.single('file'),(req,res)=>{
+app.post('/post', uploadMiddleware.single('file'), async(req,res)=>{
   const {originalname,path} = req.file;
   const parts = originalname.split('.');
   const ext = parts[parts.length -1];
   const newPath =  path+'.'+ext;
   fs.renameSync(path, newPath);
-  res.json({ext});
+
+  const {title,summary,content} = req.body;
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover: newPath,
+  }); 
+  res.json(postDoc);
 
 });
 app.listen(4000);
