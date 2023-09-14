d037a916c242ca4922bb01281afab982c404c0df
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-09-14 22:10:28 +0530

    do changes

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -50,7 +50,7 @@ export default function CreatePost(){
     const[title,setTitle] = useState('');
     const[summary,setSummary] = useState('');
     const[content,setContent] = useState('');
-    const[file,setFile] = useState('');
+    const[files,setFiles] = useState('');
     function createNewPost(ev){
         const data = new FormData();
         data.set('title', title)
@@ -58,16 +58,17 @@ export default function CreatePost(){
         data.set('content',content)
         //data.set('file',)
        ev.preventDefault();
-        fetch('http://localhost:4000/post', {
-            method :'POST',
-            //body:
-        })
+       console.log(files);
+        // fetch('http://localhost:4000/post', {
+        //     method :'POST',
+        //     body:
+        // })
     }
     return(
         <form onSubmit={createNewPost}>
            <input type="title" placeholder={'Title'} value={title} onChange={ev => setTitle(ev.target.value)}/>
            <input type="summary" placeholder={'Summary'} value={summary} onChange={ev=> setSummary(ev.target.value)}/>
-           <input type="file"/>
+           <input type="file" value={files} onChange={ev => setFiles(ev.target.files)}/>
            <ReactQuill value={content} onChange={newValue => setContent(newValue)} modules={modules} formats={formats}/>
            <button style={{marginTop:'5px'}}> Create post</button>
            </form>
