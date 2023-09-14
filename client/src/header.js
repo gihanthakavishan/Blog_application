18e782022e7f7b862e955cedbd42c35337826b73
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-09-14 16:52:24 +0530

    do some changes

--- client/src/header.js
+++ client/src/header.js
@@ -64,7 +64,7 @@ import { Link } from "react-router-dom";
 import { UserContext } from "./userContext";
 
 export default function Header() {
-  const [userInfo, setUserinfo] = useState(null); // Initialize with null or initial value
+  const [userInfo, setUserInfo] = useState(null); // Initialize with null or initial value
   const [username, setUsername] = useState(null);
 
   useEffect(() => {
@@ -73,7 +73,7 @@ export default function Header() {
     }).then(response => {
       response.json().then(userData => {
         console.log(userData);
-        setUserinfo(userData); // Update userInfo state
+        setUserInfo(userData); // Update userInfo state
         setUsername(userData.username);
       });
     });
@@ -84,7 +84,7 @@ export default function Header() {
       credentials: 'include',
       method: 'POST',
     });
-    setUserinfo(null); // Clear userInfo when logging out
+    setUserInfo(null); // Clear userInfo when logging out
   }
 
   const usersname = userInfo?.usersname;
